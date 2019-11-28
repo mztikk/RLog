b4cce754cb7dc2b510a8787319175ccda77ccb17
mztikk <nevigskcuforez@gmail.com>
2019-11-28 18:27:30 +0100

    added method to add ILogOutput to configurator

--- RLog/RLogConfigurator.cs
+++ RLog/RLogConfigurator.cs
@@ -19,29 +19,23 @@ namespace RLog
             return this;
         }
 
-        public RLogConfigurator AddConsoleOutput(LogLevel logLevel)
-        {
-            _logOutputs.Add(new ConsoleOutput(logLevel));
-            return this;
-        }
+        public RLogConfigurator AddConsoleOutput(LogLevel logLevel) => AddOutput(new ConsoleOutput(logLevel));
 
         public RLogConfigurator AddConsoleOutput() => AddConsoleOutput(_logLevel);
 
-        public RLogConfigurator AddStaticFileOutput(LogLevel logLevel, string logPath)
-        {
-            _logOutputs.Add(new StaticFileOutput(logPath, logLevel));
-            return this;
-        }
+        public RLogConfigurator AddStaticFileOutput(LogLevel logLevel, string logPath) => AddOutput(new StaticFileOutput(logPath, logLevel));
 
         public RLogConfigurator AddStaticFileOutput(string logPath) => AddStaticFileOutput(_logLevel, logPath);
 
-        public RLogConfigurator AddFileOutput(LogLevel logLevel, string logPath)
-        {
-            _logOutputs.Add(new FileOutput(logPath, logLevel));
-            return this;
-        }
+        public RLogConfigurator AddFileOutput(LogLevel logLevel, string logPath) => AddOutput(new FileOutput(logPath, logLevel));
 
         public RLogConfigurator AddFileOutput(string logPath) => AddFileOutput(_logLevel, logPath);
+
+        public RLogConfigurator AddOutput(ILogOutput logOutput)
+        {
+            _logOutputs.Add(logOutput);
+            return this;
+        }
 
         public ILogDistributor GetLogDistributor() => _logDistributor ?? new SerialDistributor(_logOutputs);
     }
